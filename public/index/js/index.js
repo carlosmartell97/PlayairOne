2a839ce0194a05251a68df86c2e2ed7f880fc636
carlosmartell97 <carlosmartell97@gmail.com>
2017-01-09 18:45:38 -0600

    now full screen is activated when clicking any of the 2 'session' buttons

--- public/index/js/index.js
+++ public/index/js/index.js
@@ -1,1 +1,17 @@
 console.log("working");
+
+// ACTIVATING FULL SCREEN:
+// Find the right method, call on correct element
+function launchIntoFullscreen(element) {
+  if(element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if(element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+};
+// launchIntoFullscreen(document.documentElement); -> the whole page
+// launchIntoFullscreen(document.getElementById("videoElement")); -> any individual element

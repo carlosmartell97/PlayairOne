1a9497fd024671adc20565151abcafcca32c663e
carlosmartell97 <carlosmartell97@gmail.com>
2017-01-13 14:26:18 -0600

    game mode now has a space for a question, possible answers and a progress bar for the time. Added icon for tab title in browsers

--- public/index/js/index.js
+++ public/index/js/index.js
@@ -18,9 +18,25 @@ function launchIntoFullscreen(element) {
 
 function startSession(nickname){
     console.log(nickname);
-    document.getElementById('gameZone').innerHTML=nickname;
-    document.getElementById('gameContainer').style.paddingTop="165px";
-    document.getElementById('portfolio').style.display="none";
-    document.getElementById('about').style.display="none";
-    document.getElementById('footer').style.display="none";
-}
+    document.getElementById('portfolio').style.display="none"; document.getElementById('about').style.display="none"; document.getElementById('footer').style.display="none";
+    document.getElementById('gameContainer').style.paddingTop="165px"; document.getElementById('gameContainer').style.height="100vh";
+    document.getElementById('gameZone').innerHTML='<div class="col-lg-12"> <div class="intro-text"> <div class="progress" style="visibility:hidden"> <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"> <span class="sr-only">0% Complete</span> </div> </div> <span class="name">Question</span> <hr class="icon"> <span class="skills"> <a class="btn btn-lg btn-outline"> Option one </a> <a class="btn btn-lg btn-outline"> Opción dos </a> <br> <a class="btn btn-lg btn-outline"> Opzione tre </a> <a class="btn btn-lg btn-outline"> Option vier </a> </span> </div> </div>';
+    $('header').append('<div style="position:absolute; bottom:0; left:0; right:0; font-size:2.5vh"> Session Code: <span id="sessionCode">45</span> </div>');
+    setTimeout("startProgressBar()",2000);
+};
+
+//setTimeout("startProgressBar()",2000); // esto debe ir dentro de startSession()
+
+function startProgressBar(){
+    $('.progress').css('visibility','visible').hide().fadeIn('slow');
+    var i = 0;
+    var progressBar = setInterval(function () {
+        i++;
+        if (i > 0) {
+            $('.progress-bar').css('width', i + '%');
+        } else {
+            clearInterval(counterBack);
+        }
+    }, 100);
+};
+

53ab6eff158f7e7125fcf85572484e37f6dccbb9
carlosmartell97 <carlosmartell97@gmail.com>
2017-01-16 13:44:24 -0600

    now writing to database. push() key, is sent from function to function. Removed capitalization of text in 'access code' section of join mode. Joining modal button checks if 'nickname' input isn't empty. If so, createSession() is called. If all goes well, startJoin() is called.

--- public/index/js/index.js
+++ public/index/js/index.js
@@ -16,12 +16,41 @@ function launchIntoFullscreen(element) {
 // launchIntoFullscreen(document.documentElement); -> the whole page
 // launchIntoFullscreen(document.getElementById("videoElement")); -> any individual element
 
+function checkInput(nickname){
+    console.log('check:'+nickname);
+    if(document.getElementById('nickname').value!=""){
+        createSession(nickname);
+    }
+    else{
+//        console.log('nopeInput');
+    }
+};
+
+function createSession(nickname){
+    //WRITING DATA
+    var ref = new Firebase("https://playairone.firebaseio.com/");
+    var sessionsRef = ref.child("sessions");
+    var pushedData = sessionsRef.push({
+            "host":nickname
+    }, function(error) {
+        if (error) {
+            console.log("Data could not be saved." + error);
+        } else {
+            console.log("Data saved successfully.");
+            console.log('pushed:'+pushedData.key());
+            $('#hostModal').modal('hide');
+            startJoin(document.getElementById('nickname').value,pushedData.key());
+        }
+    });
+    
+};
+
 function startJoin(nickname,code){
     console.log("startJoin");
     document.getElementById('portfolio').style.display="none"; document.getElementById('about').style.display="none"; document.getElementById('footer').style.display="none";
     document.getElementById('gameContainer').style.paddingTop="165px";
-    document.getElementById('gameContainer').innerHTML='<div class="row" id="gameZone"> <div class="col-lg-12"> <div class="intro-text"> <span class="name">ACCESS CODE:</span> <h1><span class="label label-warning" style="color:#000; font-size:6vw">'+code+'</span></h1> </div> <div class="intro-text" style="padding:2vw; font-size:4vh"> Playairs: </div> <div class="col-lg-12" style=" width:95%; left:2.5%"> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Default</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Sucasdfdfcess</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> </div> </div> </div> <div class="container" id="gameContainer" style="padding:8px; position:absolute; bottom:0; left:0; right:0; font-size:2.5vh"> <div class="row" id="gameZone"> <div class="col-lg-12"> <a class="btn btn-lg btn-outline" style="font-size:5vh" onclick="startCounter('+"'"+nickname+"'"+','+"'"+code+"'"+',5)"> <i class="fa fa-thumbs-up"></i> Start! </a> </div> </div> </div>';
-}
+    document.getElementById('gameContainer').innerHTML='<div class="row" id="gameZone"> <div class="col-lg-12"> <div class="intro-text"> <span class="name">ACCESS CODE:</span> <h1><span class="label label-warning" style="color:#000; font-size:6vw; text-transform:none">'+code+'</span></h1> </div> <div class="intro-text" style="padding:2vw; font-size:4vh"> Playairs: </div> <div class="col-lg-12" style=" width:95%; left:2.5%"> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Default</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Sucasdfdfcess</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primasdfsdfdary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Primary</button> <button type="button" class="btn-lg btn-primary" style="position:relative; padding:1px">Success</button> </div> </div> </div> <div class="container" id="gameContainer" style="padding:8px; position:absolute; bottom:0; left:0; right:0; font-size:2.5vh"> <div class="row" id="gameZone"> <div class="col-lg-12"> <a class="btn btn-lg btn-outline" style="font-size:5vh" onclick="startCounter('+"'"+nickname+"'"+','+"'"+code+"'"+',5)"> <i class="fa fa-thumbs-up"></i> Start! </a> </div> </div> </div>';
+};
 
 //window.onload=function() {
 //  startCounter("","",5);
@@ -50,10 +79,10 @@ function startSession(nickname,code){
     document.getElementById('gameContainer').innerHTML='<div class="row" id="gameZone" style="visibility:hidden"> <div class="col-lg-12"> <div class="intro-text"> <div class="progress" style="visibility:hidden"> <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0%"> <span class="sr-only">0% Complete</span> </div> </div> <span class="name">Question</span> <hr class="question"> <span class="skills" id="options" style="visibility:hidden"> <a class="btn btn-lg btn-outline"> Option one </a> <a class="btn btn-lg btn-outline"> Opción dos </a> <br> <a class="btn btn-lg btn-outline"> Opzione tre </a> <a class="btn btn-lg btn-outline"> Option vier </a> </span> </div> </div> </div>';
     $('header').append('<div style="position:absolute; bottom:0; left:0; right:0; font-size:2.5vh"> Session Code: <span id="sessionCode">'+code+'</span> </div>');
     $('#gameZone').css('visibility','visible').hide().fadeIn('slow');
-    setTimeout("startProgressBar()",2000);
+    setTimeout("startQuestion()",2000);
 };
 
-function startProgressBar(){
+function startQuestion(){
     $('.progress').css('visibility','visible').hide().fadeIn('slow');
     $('#options').css('visibility','visible').hide().fadeIn('slow');
     var i = 0;
@@ -66,4 +95,3 @@ function startProgressBar(){
         }
     }, 100);
 };
-
